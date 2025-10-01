8cb8cdb08c56fa2e9a4a3d45a35e2d82b57e2310
shubhankar-mern <shubhankars361@gmail.com>
2025-10-01 21:39:06 +0530

    Ft:Updated resume link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -108,7 +108,7 @@ const Navbar = () => {
               </a>
             ))}
             <a
-              href="https://drive.google.com/file/d/1p2xKbi9sDsKt_iVifv-DpT_zfMvUIroF/view?usp=drive_link"
+              href="https://drive.google.com/file/d/1W8lG0LfJggqkExPR_izUYKr67eBCAp-w/view?usp=sharing"
               style={resumeButtonStyle}
               className="hover:bg-[#64ffda]/10 hover:translate-y-[-2px]"
               target="_blank"
